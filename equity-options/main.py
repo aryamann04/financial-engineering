8f4332d74e1d3d083c0d7dbc8cc488c22ae4af9f
aryamann04 <140534650+aryamann04@users.noreply.github.com>
2024-06-22 20:48:17 +0900

    Update main.py

--- equity-options/main.py
+++ equity-options/main.py
@@ -1,30 +1,54 @@
 from optionstrategies import OptionStrategy
+from currentbonds import treasury_yield
 
 #-----------------------------------------------------------#
 ticker = "AAPL"
-r = 0.05 # risk-free rate (annual)
-T = 1.5  # years
-n = 10 # number of periods in the binomial model
-percent_itm_otm = 0.1 # for option strategies (0, 1)
+T = 0.25  # years
+r = treasury_yield(T)  # risk-free rate (annual)
+n = 10  # number of periods in the binomial model
+percent_itm_otm = 0.1  # for option strategies
 #-----------------------------------------------------------#
 
 # create a strategy object and call the relevant strategy function
-AAPL_strategy = OptionStrategy(ticker, percent_itm_otm, T, r, n)
-AAPL_strategy.long_strangle()
+strategy = OptionStrategy(ticker, percent_itm_otm, T, r, n)
+strategy.iron_condor()
 
-AAPL_strategy.strategy_price() # print Black-Scholes and market price
-AAPL_strategy.greeks() # print strategy greeks
-AAPL_strategy.visualize_payoff() # view payoff graph and break-even points
+strategy.strategy_price()  # print Black-Scholes and market price
+strategy.greeks()  # print strategy greeks
+strategy.visualize_payoff()  # view payoff graph and break-even points
 
 '''
 available option strategies: 
-1.  covered_call()
-2.  married_put()
-3.  bull_call_spread()
-4.  bear_put_spread()
-5.  protective_collar()
-6.  long_straddle()
-7.  long_strangle()
-8.  long_call_butterfly_spread()
-9.  long_put_butterfly_spread()
+
+*   atm_call()
+*   itm_call()
+*   otm_call()
+*   short_atm_call()
+*   short_itm_call()
+*   short_otm_call()
+
+*   atm_put()
+*   itm_put()
+*   otm_put()
+*   short_atm_put()
+*   short_itm_put()
+*   short_otm_put()
+
+*   covered_call()
+*   married_put()
+
+*   bull_call_spread()
+*   bear_put_spread()
+*   credit_call_spread()
+*   credit_put_spread() 
+
+*   protective_collar()
+*   long_straddle()
+*   long_strangle()
+*   short_straddle()
+*   short_strangle()
+
+*   long_call_butterfly_spread()
+*   short_call_butterfly_spread()
+*   iron_condor()
 '''
