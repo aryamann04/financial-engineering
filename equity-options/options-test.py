5f2f4f4f0fd5c57974005e84c19bb7db60cde725
aryamann04 <140534650+aryamann04@users.noreply.github.com>
2024-06-25 18:15:42 +0900

    Update options-test.py

--- equity-options/options-test.py
+++ equity-options/options-test.py
@@ -1,21 +1,30 @@
 from optionstrategies import OptionStrategy
 from currentbonds import treasury_yield
+from exotics import DigitalOption
 
 #-----------------------------------------------------------#
-ticker = "AAPL"
-T = 0.25  # years
+ticker = "AMZN"
+T = 5  # years
 r = treasury_yield(T)  # risk-free rate (annual)
 n = 10  # number of periods in the binomial model
-percent_itm_otm = 0.1  # for option strategies
+percent_itm_otm = 0.2  # for option strategies
 #-----------------------------------------------------------#
 
 # create a strategy object and call the relevant strategy function
 strategy = OptionStrategy(ticker, percent_itm_otm, T, r, n)
-strategy.iron_condor()
+strategy.bull_call_spread()
 
 strategy.strategy_price()  # print Black-Scholes and market price
 strategy.greeks()  # print strategy greeks
 strategy.visualize_payoff()  # view payoff graph and break-even points
+
+#-----------------------------------------------------------#
+digital_option_strike = 150
+option_type = "call"
+#-----------------------------------------------------------#
+
+digital_call_option = DigitalOption(ticker, r, T, digital_option_strike, option_type)
+digital_call_option.price()
 
 '''
 available option strategies: 
