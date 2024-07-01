13ff500fd7d93695f5de736719b41dacf3a0cf71
aryamann04 <140534650+aryamann04@users.noreply.github.com>
2024-07-01 21:37:36 +0900

    add asian options

--- equity-options/exotics.py
+++ equity-options/exotics.py
@@ -1,10 +1,12 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-from optionspricing import div_yield, stock_data
-from scipy.stats import norm
 import numpy as np
-from optionspricing import stock_data, bs_price
+
+from scipy.stats import norm
+from datetime import datetime, timedelta
+
+from optionspricing import div_yield, stock_data, bs_price
+from montecarlo import monte_carlo_digital, monte_carlo_range_accrual, monte_carlo_asian
 
 class DigitalOption:
     def __init__(self, ticker, r, T, K, option_type="call", payoff_amount=1, position="long", creation_date=None):
@@ -15,13 +17,15 @@ class DigitalOption:
         self.option_type = option_type
         self.position = position
         self.creation_date = creation_date
-
         self.payoff_amount = payoff_amount
+
         self.S_0, self.sigma = stock_data(ticker, creation_date)
+        self.q = div_yield(ticker)
         self.bs_price = self.digital_option_bs_price()
+        self.mc_price = self.monte_carlo_price()
 
     def _x(self):
-        x = (np.log(self.K / self.S_0) - (self.r - 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+        x = (np.log(self.K / self.S_0) - (self.r - self.q - 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
         return x
 
     def digital_option_bs_price(self):
@@ -31,8 +35,13 @@ class DigitalOption:
         else:
             return np.exp(-self.r * self.T) * norm.cdf(x) * self.payoff_amount
 
+    def monte_carlo_price(self):
+        return monte_carlo_digital(self.S_0, self.K, self.T, self.r, self.q, self.sigma, self.option_type)
+
     def price(self):
-        return print(f"{self.ticker} Digital {self.option_type} with strike {self.K}: ${self.bs_price:.2f}")
+        print(f"{self.ticker} Digital {self.option_type} with strike {self.K} (Black-Scholes): ${self.bs_price:.2f}")
+        print(f"{self.ticker} Digital {self.option_type} with strike {self.K} (Monte Carlo): ${self.mc_price:.2f}")
+
 
     def visualize_payoff(self):
         stock_prices = np.linspace(self.S_0 * 0.5, self.S_0 * 1.5, 1000)
@@ -63,7 +72,15 @@ class SinglePeriodRangeAccrual:
         self.K_low = K_low
         self.K_up = K_up
         self.coupon = coupon
+
+        self.S_0, self.sigma = stock_data(ticker)
+        self.q = div_yield(ticker)
         self.bs_price = self.range_accrual_bs_price()
+        self.mc_price = self.monte_carlo_price()
+
+    def monte_carlo_price(self):
+        return monte_carlo_range_accrual(self.S_0, self.K_low, self.K_up,
+                                         self.T, self.r, self.q, self.sigma, self.coupon)
 
     def range_accrual_bs_price(self):
         digital_call_low = DigitalOption(self.ticker, self.r, self.T, self.K_low, option_type="call")
@@ -75,7 +92,8 @@ class SinglePeriodRangeAccrual:
         return self.coupon * (price_low - price_up)
 
     def price(self):
-        print(f"{self.ticker} Single-period range accrual {self.K_low}-{self.K_up}: ${self.bs_price:.2f}")
+        print(f"{self.ticker} Single-period range accrual {self.K_low}-{self.K_up} (Black-Scholes): ${self.bs_price:.2f}")
+        print(f"{self.ticker} Single-period range accrual {self.K_low}-{self.K_up} (Monte Carlo): ${self.mc_price:.2f}")
 
     def visualize_payoff(self):
         stock_prices = np.linspace(self.K_low * 0.5, self.K_up * 1.5, 1000)
@@ -96,6 +114,24 @@ class SinglePeriodRangeAccrual:
         plt.title(f'{self.ticker} Single-period Range Accrual Profit/Loss')
         plt.legend()
         plt.show()
+
+class AsianOption:
+    def __init__(self, ticker, r, T, K, option_type="call"):
+        self.ticker = ticker
+        self.r = r
+        self.T = T
+        self.K = K
+        self.option_type = option_type
+
+        self.S_0, self.sigma = stock_data(ticker)
+        self.q = div_yield(ticker)
+        self.mc_price = self.monte_carlo_price()
+
+    def monte_carlo_price(self):
+            return monte_carlo_asian(self.S_0, self.K, self.T, self.r, self.q, self.sigma, self.option_type)
+
+    def price(self):
+        print(f"{self.ticker} Asian option with strike {self.K} (Monte Carlo): ${self.mc_price:.2f}")
 
 def get_iv(tic, K, T, option_type):
     ticker = yf.Ticker(tic)
